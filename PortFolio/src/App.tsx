ced6191fa4e4bbaa1c605616f44a9c67b7b5719c
박병조 <k97115284@gmail.com>
2024-05-28 01:12:35 +0800

    [Work]2024.05.28

--- PortFolio/src/App.tsx
+++ PortFolio/src/App.tsx
@@ -1,8 +1,22 @@
+import Profile from "./components/Profile"
+import Carousel from "./components/Carousel"
+import Skills from "./components/Skills"
+// import Project1 from "./components/Project1"
+// import Project2 from "./components/Project2"
+// import Project3 from "./components/Project3"
+import ProjectSummary from "./components/ProjectSummary"
 
 function App() {
   return (
-    <div className="bg-gray">
-      <p className="text-lg font-gtr-B">tesssat</p>
+    <div>
+      <Carousel>
+        <Profile />
+        <Skills />
+        <ProjectSummary goToSlide={() => {}} />
+        {/* <Project1 />
+        <Project2 />
+        <Project3 /> */}
+      </Carousel>
     </div>
   )
 }
