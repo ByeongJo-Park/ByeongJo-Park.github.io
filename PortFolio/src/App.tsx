9b463220923c2dc43df32006d005d863e1436d53
박병조 <k97115284@gmail.com>
2024-11-16 19:24:41 +0900

    [feat] 폰트 업데이트 및 포트폴리오 리뉴얼 시작

--- PortFolio/src/App.tsx
+++ PortFolio/src/App.tsx
@@ -1,23 +1,10 @@
-import Profile from "./components/Profile"
-import Carousel from "./components/Carousel"
-import Skills from "./components/Skills"
-// import Project1 from "./components/Project1"
-// import Project2 from "./components/Project2"
-// import Project3 from "./components/Project3"
-import ProjectSummary from "./components/ProjectSummary"
+import React from 'react';
+import Main from './pages/Main';
 
-function App() {
+
+const App: React.FC = () => {
   return (
-    <div>
-      <Carousel>
-        <Profile />
-        <Skills />
-        <ProjectSummary goToSlide={() => {}} />
-        {/* <Project1 />
-        <Project2 />
-        <Project3 /> */}
-      </Carousel>
-    </div>
+      <Main/>
   )
 }
 
