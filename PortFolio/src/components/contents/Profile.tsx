047c9cf1773ee464619aba8afebb9200f1d54e52
박병조 <k97115284@gmail.com>
2024-12-18 17:46:59 +0900

    [feat] 프로필페이지 작성

--- PortFolio/src/components/contents/Profile.tsx
+++ PortFolio/src/components/contents/Profile.tsx
@@ -4,7 +4,7 @@ import profilePhoto from '../../assets/IDPhoto.jpg';
 const Profile: React.FC = () => {
     return (
         <div className="w-full bg-light-black border-point-gray m-2 rounded-3xl p-16 text-white">
-            <div className='w-full'>
+            <div className='w-full flex flex-col gap-4'>
                 <div className='w-full flex'>
                     <img src={profilePhoto} alt='Profile' className='w-40 h-40 rounded-full object-cover object-top'/>
                     <div className='flex flex-col p-8 h-44 justify-center'>
@@ -15,8 +15,8 @@ const Profile: React.FC = () => {
                 <div className='w-full flex flex-col gap-3'>
                     <span className='font-frs-eb text-3xl'>Skill</span>
                     <div className='flex flex-col px-4'>
-                        <span className='p-1 font-frs-b'></span>
-                        <div className='flex gap-1 m-3'>
+                        <li className='p-1 font-frs-b'>활용해서 작업이 즉시 가능해요.</li>
+                        <div className='flex gap-1 m-3 my-1'>
                             <img alt="Static Badge" src="https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=React&logoColor=white"/>
                             <img alt="Static Badge" src="https://img.shields.io/badge/typescript-3178C6?style=for-the-badge&logo=typescript&logoColor=white"/>
                             <img alt="Static Badge" src="https://img.shields.io/badge/javascript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=white"/>
@@ -27,23 +27,32 @@ const Profile: React.FC = () => {
                         </div>
                     </div>
                     <div className='flex flex-col px-4'>
-                        <span className='p-1 font-frs-b'></span>
-                        <div className='flex gap-1 m-3'>
+                        <li className='p-1 font-frs-b'>다뤄본 경험이 있어요.</li>
+                        <div className='flex gap-1 m-3 my-1'>
                             <img alt="Static Badge" src="https://img.shields.io/badge/jirasoftware-0052CC?style=for-the-badge&logo=jirasoftware&logoColor=white"/>
                             <img alt="Static Badge" src="https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jQuery&logoColor=white"/>
                             <img alt="Static Badge" src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=Python&logoColor=white"/>
                         </div>
                     </div>
-
                 </div>
                 <div className='w-full flex flex-col'>                
-                    <span className='font-frs-eb text-3xl'>Career</span>
+                    <span className='font-frs-eb text-3xl'>Career ( 총 1년 3개월 )</span>
+                    <div className='flex flex-col px-4'>
+                        <li className='p-1 font-frs-b text-xl'>티앤테크 ( 솔루션 개발 2팀 / 1년 3개월 근무 )</li>
+                    </div>
                 </div>
                 <div className='w-full flex flex-col'>
                     <span className='font-frs-eb text-3xl'>Education</span>
+                    <div className='flex flex-col px-4'>
+                        <li className='p-1 font-frs-b text-xl'>영남대학교 전기공학과 졸업 ( 2014.03 ~ 2022.02 )</li>
+                        <li className='p-1 font-frs-b text-xl'>Samsung Software Academy For Youth 10기 수료 ( 2023.07 ~ 2024.06 )</li>
+                    </div>
                 </div>
                 <div className='w-full flex flex-col'>
                     <span className='font-frs-eb text-3xl'>Certificate</span>
+                    <div className='flex flex-col px-4'>
+                        <li className='p-1 font-frs-b text-xl'>정보처리기사 ( 2024.09 취득 )</li>
+                    </div>
                 </div>
             </div>
         </div>
