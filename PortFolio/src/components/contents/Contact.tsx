bc32d3c664eed8ebb9c5ffbd6eb0d90407c316ea
Park.Pat <90613277+ByeongJo-Park@users.noreply.github.com>
2025-01-17 01:15:58 +0900

    [test] 페이지 레이아웃 변경 Contact.tsx 업데이트

--- PortFolio/src/components/contents/Contact.tsx
+++ PortFolio/src/components/contents/Contact.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Contact: React.FC = () => {
     return (
-        <div className="w-full h-80 m-2 flex flex-col gap-2 items-center justify-center text-white font-frs-bl">
+            <div className="w-full bg-light-black border border-point-gray m-2 rounded-3xl px-6 py-12 text-white">
             <span className='text-2xl'>Email : k97115284@gmail.com</span>
             <span className='text-xl'>Mobile : 010 - 9711 - 5284</span>
         </div>
