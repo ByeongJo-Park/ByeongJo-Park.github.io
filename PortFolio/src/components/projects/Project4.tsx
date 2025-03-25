6e7910ecc84b92505a4d6db9974a7696902e2da3
박병조 <k97115284@gmail.com>
2025-03-25 22:29:44 +0900

    페이지 수정

--- PortFolio/src/components/projects/Project4.tsx
+++ PortFolio/src/components/projects/Project4.tsx
@@ -6,10 +6,10 @@ const Project3: React.FC = () => {
     <div className="w-full border border-point-gray rounded-xl bg-light-black font-frs-b p-4 text-white">
       <img src={logo} alt="pjtLogo" className='p-4'/>
       <div className='w-full p-4'>
-        <p className='text-3xl mb-4'>Ukkikki</p>
-        <p className='font-pre-B mb-1'>역할 : 팀장 / Front-End Dev</p>
-        <p className='font-pre-M'>S3 서버 암호화 + 간편 비밀번호를 통한 다중 보안</p>
-        <p className='font-pre-M'>촬영한 사진을 즉시 업로드 및 공유 앨범 서비스</p>
+        <p className='text-3xl mb-4'>곁에</p>
+        <p className='font-pre-B mb-1'>역할 : Front-End Dev</p>
+        <p className='font-pre-M'>요양보호사와 노인의 최적 매칭을 통해</p>
+        <p className='font-pre-M'>재가노인복지센터의 구인난을 해결하고, 맞춤형 돌봄 서비스를 제공하는 매칭 플랫폼</p>
       </div>
       <div className="w-full p-4 py-2">
         <p className='mb-2'>주요 개발사항</p>
