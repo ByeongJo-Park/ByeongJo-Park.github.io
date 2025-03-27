a76cb75418b682b179c475bea97c75bb3d88dce8
박병조 <k97115284@gmail.com>
2025-03-27 16:03:02 +0900

    fix: test

--- PortFolio/tailwind.config.js
+++ PortFolio/tailwind.config.js
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+import scrollbarHide from 'tailwind-scrollbar-hide';
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
@@ -8,37 +9,42 @@ export default {
   theme: {
     extend: {
       colors: {
-        "sky-blue"      : "#88C6FF",
-        "yellow"        : "#FFCE21",
-        "gray"          : "#BDBDBD",
-        "point-gray"    : "#5C5C5C",
-        "white"         : "#FFFFFF",
-        "red"           : "#E71C1C",
-        "black"         : "#171717",
-        "light-black"   : "#191919",
-        "green"         : "#52DD22",
+        "sky-blue": "#88C6FF",
+        "yellow": "#FFCE21",
+        "gray": "#BDBDBD",
+        "point-gray": "#5C5C5C",
+        "white": "#FFFFFF",
+        "red": "#E71C1C",
+        "black": "#171717",
+        "light-black": "#191919",
+        "green": "#52DD22",
       },
-      fontFamily:{
-        'frs-th' : ['frs-th'],
-        'frs-el' : ['frs-el'],
-        'frs-l' : ['frs-l'],
-        'frs-r' : ['frs-r'],
-        'frs-m' : ['frs-m'],
-        'frs-sb' : ['frs-sb'],
-        'frs-b' : ['frs-b'],
-        'frs-eb' : ['frs-eb'],
-        'frs-bl' : ['frs-bl'],
+      fontFamily: {
+        'frs-th': ['frs-th'],
+        'frs-el': ['frs-el'],
+        'frs-l': ['frs-l'],
+        'frs-r': ['frs-r'],
+        'frs-m': ['frs-m'],
+        'frs-sb': ['frs-sb'],
+        'frs-b': ['frs-b'],
+        'frs-eb': ['frs-eb'],
+        'frs-bl': ['frs-bl'],
       },
       keyframes: {
         'custom-bounce': {
           '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-10px)'},
+          '50%': { transform: 'translateY(-10px)' },
+        },
       },
       animation: {
-        'custom-bounce':'customBounce 1.5s infinite',
+        'custom-bounce': 'custom-bounce 1.5s infinite',
       },
     },
   },
-  plugins: [],
-  }
-}
+  plugins: [
+    scrollbarHide
+  ],
+  corePlugins: {
+    preflight: true,
+  },
+};
