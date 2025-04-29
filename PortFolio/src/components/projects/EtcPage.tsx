7a073b6939f53ab2bdb48e4fb2eb1f638af50de8
박병조 <k97115284@gmail.com>
2025-04-29 19:03:08 +0900

    fix: etc. 페이지 수정

--- PortFolio/src/components/projects/EtcPage.tsx
+++ PortFolio/src/components/projects/EtcPage.tsx
@@ -13,12 +13,10 @@ const Project1: React.FC = () => {
         <p className='font-pre-M'>실시간 문서 동시편집 및 일정공유 서비스</p>
         <p className='mb-4 text-xl font-semibold'>프론트엔드 주요 구현 내용</p>
         <ul className='list-disc list-inside space-y-1'>
-          <li>Recoil 기반 상태 관리 도입 및 일정 데이터의 전역 상태 연동</li>
-          <li>달력 UI 커스텀 및 팀 일정 연동 로직 구현 (React + TypeScript)</li>
+          <li>달력 UI 구현현 및 팀 일정 연동 로직 구현 (Vue + JS)</li>
           <li>팀 일정 등록/수정 폼 컴포넌트 분리 및 모달 기반 UX 구현</li>
           <li>비동기 API 통신 구조 설계 및 Axios Interceptor를 통한 에러 핸들링</li>
-          <li>팀 생성/수정/삭제 기능을 위한 동적 라우팅 및 Form Validation 적용</li>
-          <li>반응형 UI 구현 및 사용자 경험(UX) 개선을 위한 애니메이션 도입</li>
+          <li>팀 생성/수정/삭제 컴포넌트 구현</li>
         </ul>
       </section>
       <Divider/>
